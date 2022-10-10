63196745f0dd01fe1f168c55fde098f035482a0c
Eяw!и Gontier <ierwinx@me.com>
2022-10-10 16:36:55 -0500

    Initial Commit

---
+++ ExpresionesRegulares/ExpresionesRegularesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpresionesRegularesApp.swift
+//  ExpresionesRegulares
+//
+//  Created by Erwin Luz León on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpresionesRegularesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
